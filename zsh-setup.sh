adca956f0f29be464152aded0d08c9bb2d08ca23
Faris Aziz <farisaziz12@gmail.com>
2021-03-25 22:36:07 +0100

    fix(zsh-sh): remove cd

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,4 +1,3 @@
-cd
 git clone https://github.com/denysdovhan/spaceship-prompt.git --depth=1 ~/spaceship
 ln -sf "$PWD/spaceship.zsh" "/usr/local/share/zsh/site-functions/prompt_spaceship_setup"
 touch ~/.zshrc
