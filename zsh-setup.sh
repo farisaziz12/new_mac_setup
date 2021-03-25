8483002239f6cd12f4fc0f71971e5ee43d54334d
Faris Aziz <farisaziz12@gmail.com>
2021-03-25 22:34:58 +0100

    fix(zsh-sh): fix clone dir

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,5 +1,5 @@
 cd
-git clone https://github.com/denysdovhan/spaceship-prompt.git --depth=1
+git clone https://github.com/denysdovhan/spaceship-prompt.git --depth=1 ~/spaceship
 ln -sf "$PWD/spaceship.zsh" "/usr/local/share/zsh/site-functions/prompt_spaceship_setup"
 touch ~/.zshrc
 cp ./zsh.txt ~/.zshrc
