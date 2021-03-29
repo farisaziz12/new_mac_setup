96ff1756c35f2e3ab5008b4cbdc420e8e3024b9a
Full Name <farisaziz12@gmail.com>
2021-03-29 14:04:09 +0200

    feat(zsh): delete files and move to main setup script

--- zsh-setup.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/denysdovhan/spaceship-prompt.git --depth=1 ~/spaceship
-ln -sf "$PWD/spaceship.zsh" "/usr/local/share/zsh/site-functions/prompt_spaceship_setup"
-touch ~/.zshrc
-cp ./zsh.txt ~/.zshrc
